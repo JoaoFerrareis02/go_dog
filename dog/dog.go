d1dddf7ba61d9c829eee1e32c6a2c0a6620d84f3
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-01-07 09:38:29 -0300

    first commit

---
+++ dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog implements functions reffering dogs
+package dog
+
+// Year takes human years and turns them into dog years (1 human year = 7 dog years)
+func Years(y int) int {
+	return y * 7
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoaoFerrareis02/go_dog
+
+go 1.21.5
